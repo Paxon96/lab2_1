dcdf0214ac8446e8ac4782d011d8cb8b6a9a682d
wloczyk.bartlomiej <bwloczyk@gmail.com>
2019-03-23 21:20:46 +0100

    testy_v1 test isFound method & fix getPosition from BinarySearch

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -12,6 +12,7 @@ public class BinarySearchTest {
         int[] sequence = {ELEMENT};
 
         Assert.assertTrue(BinarySearch.search(ELEMENT, sequence).isFound());
+        Assert.assertEquals(ELEMENT, sequence[BinarySearch.search(ELEMENT, sequence).getPosition()]);
     }
 
     @Test
@@ -19,32 +20,36 @@ public class BinarySearchTest {
         int[] sequence = {ELEMENT + 1};
 
         Assert.assertFalse(BinarySearch.search(ELEMENT, sequence).isFound());
+        Assert.assertEquals(-1, BinarySearch.search(ELEMENT, sequence).getPosition());
     }
 
     @Test
     public void elementFirstInSequenceTest() {
         int[] sequence = {ELEMENT, ELEMENT + 1, ELEMENT + 2, ELEMENT + 3};
 
-        Assert.assertEquals(1, BinarySearch.search(ELEMENT, sequence).getPosition());
+        Assert.assertTrue(BinarySearch.search(ELEMENT, sequence).isFound());
+        Assert.assertEquals(ELEMENT, sequence[BinarySearch.search(ELEMENT, sequence).getPosition()]);
     }
 
     @Test
     public void elementLastInSequenceTest() {
         int[] sequence = {ELEMENT - 3, ELEMENT - 2, ELEMENT - 1, ELEMENT};
 
-        Assert.assertEquals(sequence.length, BinarySearch.search(ELEMENT, sequence).getPosition());
+       Assert.assertTrue(BinarySearch.search(ELEMENT, sequence).isFound());
+       Assert.assertEquals(ELEMENT, sequence[BinarySearch.search(ELEMENT, sequence).getPosition()]);
     }
 
     @Test
     public void elementMiddleInSequenceTest() {
         int[] sequence = {ELEMENT - 2, ELEMENT - 1, ELEMENT, ELEMENT + 1, ELEMENT + 2};
-
-        Assert.assertEquals((sequence.length/2) +1, BinarySearch.search(ELEMENT, sequence).getPosition());
+        Assert.assertTrue(BinarySearch.search(ELEMENT, sequence).isFound());
+        Assert.assertEquals(ELEMENT, sequence[BinarySearch.search(ELEMENT, sequence).getPosition()]);
     }
 
     @Test
     public void elementNotInMultiSequenceTest() {
         int[] sequence = {ELEMENT + 1, ELEMENT + 2, ELEMENT + 3};
         Assert.assertFalse(BinarySearch.search(ELEMENT, sequence).isFound());
+        Assert.assertEquals(-1, BinarySearch.search(ELEMENT, sequence).getPosition());
     }
 }
