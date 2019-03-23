11cf2e47144faecec1ef78f61f4ccb074cd5a2b5
wloczyk.bartlomiej <bwloczyk@gmail.com>
2019-03-23 22:09:39 +0100

    testy_v3 add test searching element in sequence containing the same elements

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -22,7 +22,7 @@ public class BinarySearchTest {
     public void elementNotInSingleSequenceTest() {
         int[] sequence = {ELEMENT + 1};
 
-        Assert.assertThat(false,is(equalTo((BinarySearch.search(ELEMENT, sequence).isFound()))));
+        Assert.assertThat(false, is(equalTo((BinarySearch.search(ELEMENT, sequence).isFound()))));
         Assert.assertThat(-1, is(equalTo(BinarySearch.search(ELEMENT, sequence).getPosition())));
     }
 
@@ -40,8 +40,7 @@ public class BinarySearchTest {
         Assert.assertThat(ELEMENT, is(equalTo(sequence[BinarySearch.search(ELEMENT, sequence).getPosition()])));
     }
 
-    @Test
-    public void elementMiddleInSequenceTest() {
+    @Test public void elementMiddleInSequenceTest() {
         int[] sequence = {ELEMENT - 2, ELEMENT - 1, ELEMENT, ELEMENT + 1, ELEMENT + 2};
 
         Assert.assertThat(ELEMENT, is(equalTo(sequence[BinarySearch.search(ELEMENT, sequence).getPosition()])));
@@ -55,17 +54,24 @@ public class BinarySearchTest {
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void searchElementInEmptySequenceTest(){
+    public void searchElementInEmptySequenceTest() {
         int[] sequence = {};
 
-        BinarySearch.search(ELEMENT,sequence);
+        BinarySearch.search(ELEMENT, sequence);
     }
 
     @Test
-    public void searchNegativeElementInSequenceTest(){
+    public void searchNegativeElementInSequenceTest() {
         int[] sequence = {ELEMENT - 4, ELEMENT - 3, ELEMENT - 2, ELEMENT - 1, ELEMENT, ELEMENT + 1, ELEMENT + 2};
         int key = ELEMENT - 4;
 
         Assert.assertThat(key, is(equalTo(sequence[BinarySearch.search(key, sequence).getPosition()])));
+    }
+
+    @Test
+    public void searchElementInSequenceContainsSameElementsTest() {
+        int[] sequence = {ELEMENT, ELEMENT, ELEMENT, ELEMENT, ELEMENT, ELEMENT, ELEMENT, ELEMENT};
+
+        Assert.assertThat((sequence.length - 1) / 2, is(equalTo(BinarySearch.search(ELEMENT, sequence).getPosition())));
     }
 }
